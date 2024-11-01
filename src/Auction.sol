3e4516d65c972918ffe3eeceb170a1a55257abd8
felix <felixcjy971231@gmail.com>
2024-11-01 09:22:12 +0800

    first commit

--- src/Auction.sol
+++ src/Auction.sol
@@ -4,143 +4,137 @@ pragma solidity ^0.8.24;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-// 状态加s_前缀.所有状态用internal
-contract Auction is IERC721Receiver {
-    error CannotWithdrawHighestBid();
-    error NothingToWithdraw();
-    error FailedToWithdrawBid(address bidder, uint256 amount);
-    error BidNotHighEnough();
-    error Error_AuctionEnded();
-    error FailedToSendEth(address recipient, uint256 amount);
-    error NowAuctionsInProgress();
+contract Auction is IERC721Receiver, ReentrancyGuard {
     error TooEarlyToEnd();
+    error BidNotHighEnough();
+    error NothingToWithdraw();
+    error Error_AuctionEnded();
+    error BiddingTimeTooShort();
     error OnlyERC721Supported();
-    error Cooling();
+    error NoAuctionsInProgress();
+    error CannotWithdrawHighestBid();
+    error FailedToSendEth(address recipient, uint256 amount);
+    error FailedToWithdrawBid(address bidder, uint256 amount);
+    error InvalidTokenId();
+    error OnlySellerCanCancel();
+    error AuctionNotStarted();
 
-    address internal immutable i_seller;
-    address internal immutable i_ERC721;
-    uint256 internal immutable i_bidCD;
-    // Cool Down Time
+    address public immutable i_seller;
+    address public immutable i_ERC721;
+    uint256 public immutable i_tokenId;
+    uint256 public immutable i_minBidIncrement;
 
-    bool internal s_started;
-    // s_ended?
-    uint256 internal s_endTimestamp;
-    address internal s_highestBidder;
-    uint256 internal s_highestBid;
-    uint256 internal s_tokenId;
+    bool public s_started;
+    bool public s_ended;
+    uint256 public s_endTimestamp;
 
-    mapping(address bidder => uint256 totalBiddedEth) s_pendingReturns;
+    address public s_highestBidder;
+    uint256 public s_highestBid;
+
+    mapping(address bidder => uint256 totalBiddedETH) public s_bids;
 
     event AuctionStarted(uint256 indexed tokenId, uint256 indexed endTimestamp);
     event HighestBidincreased(address indexed bidder, uint256 indexed amount);
     event AuctionEnded(address indexed winner, uint256 indexed winningBid);
+    event AuctionCancelled();
 
-    // 拍卖由卖家开启
-    // 合约接收token，确保拍品在合约的控制内。
-    // 未设置拍品
     constructor(
         address _erc721Address,
-        uint256 _biddingtime,
-        uint256 _bidCD,
-        uint256 _startingBid,
         uint256 _tokenId,
+        uint256 _startingBid,
+        uint256 _biddingtime,
+        uint256 _minBidIncrement,
         bytes calldata data
     ) {
-        // 至少1小时
-        require(_biddingtime >= 3600);
+        if (_biddingtime < 1 hours) revert BiddingTimeTooShort();
+
+        IERC721(_erc721Address).safeTransferFrom(msg.sender, address(this), _tokenId, data);
 
         i_seller = msg.sender;
         i_ERC721 = _erc721Address;
-
-        // 拍品给到拍卖平台
-        IERC721(i_ERC721).safeTransferFrom(i_seller, address(this), _tokenId, data);
+        i_tokenId = _tokenId;
+        i_minBidIncrement = _minBidIncrement;
 
         s_started = true;
         s_endTimestamp = block.timestamp + _biddingtime;
-        // bidding time竞标时间
-
-        s_tokenId = _tokenId;
-        i_bidCD = _bidCD;
-
         s_highestBidder = msg.sender;
         s_highestBid = _startingBid;
-        // 起拍价
 
         emit AuctionStarted(_tokenId, s_endTimestamp);
     }
 
-    // 查询
-    // function getTokenIdOnAuction() external view returns (uint256) {
-    //     return s_tokenIdOnAuction;
-    // }
+    function bid() external payable nonReentrant {
+        if (!s_started) revert AuctionNotStarted();
+        if (s_ended || block.timestamp >= s_endTimestamp) revert Error_AuctionEnded();
+        if (msg.value <= s_highestBid + i_minBidIncrement) revert BidNotHighEnough();
 
-    function bid() external payable {
-        if (!s_started) revert NowAuctionsInProgress();
-        if (block.timestamp >= s_endTimestamp) revert Error_AuctionEnded();
-        if (msg.value <= s_highestBid) revert BidNotHighEnough();
-        if(i_bidCD) revert Cooling();
+        if (s_endTimestamp - block.timestamp < 1 minutes) {
+            s_endTimestamp += 3 minutes;
+        }
 
-        // 把之前最高竞标者和竞标价存进回款映射
-        if (s_highestBid > 0) {
-            s_pendingReturns[s_highestBidder] += s_highestBid;
+        if (s_highestBidder != address(0)) {
+            s_bids[s_highestBidder] += s_highestBid;
         }
 
         s_highestBidder = msg.sender;
         s_highestBid = msg.value;
-        // s_bids[msg.sender] += msg.value;
 
         emit HighestBidincreased(msg.sender, msg.value);
     }
-    /* 拍卖必须已经开始，且未结束（检查当前时间是否超过结束时间戳）。
-    新的出价必须高于当前最高出价。 */
 
-    function getBidCd
-
-    // 撤回出价
-    function withdrawBid() external {
+    function withdrawBid() external nonReentrant {
         if (msg.sender == s_highestBidder) revert CannotWithdrawHighestBid();
 
-        uint256 amount = s_pendingReturns[msg.sender];
+        uint256 amount = s_bids[msg.sender];
         if (amount == 0) revert NothingToWithdraw();
-        delete s_pendingReturns[msg.sender];
+        delete s_bids[msg.sender];
 
         (bool sent,) = msg.sender.call{value: amount}("");
         if (!sent) revert FailedToWithdrawBid(msg.sender, amount);
     }
-    // 当前最高出价者不能调用,防止拍卖无效
-    // call 是一个推荐的安全发送 ETH 的方法，因为它处理了潜在的 gas 限制问题。
 
-    // 拍卖结束
-    function endAuction() public {
+    function endAuction() public nonReentrant {
         // 拍卖要已经开始, 当前时间超过结束时间
-        if (!s_started) revert NowAuctionsInProgress();
+        if (!s_started) revert AuctionNotStarted();
+        if (s_ended) revert Error_AuctionEnded();
         if (block.timestamp < s_endTimestamp) revert TooEarlyToEnd();
 
-        s_started = false;
+        s_ended = true;
 
-        IERC721(i_ERC721).safeTransferFrom(address(this), s_highestBidder, s_tokenId, "");
-
-        (bool sent,) = i_seller.call{value: s_highestBid}("");
-        if (!sent) revert FailedToSendEth(i_seller, s_highestBid);
+        if (s_highestBidder != address(0)) {
+            IERC721(i_ERC721).safeTransferFrom(address(this), s_highestBidder, i_tokenId, "");
+            (bool sent,) = i_seller.call{value: s_highestBid}("");
+            if (!sent) revert FailedToSendEth(i_seller, s_highestBid);
+        } else {
+            IERC721(i_ERC721).safeTransferFrom(address(this), i_seller, i_tokenId, "");
+        }
 
         emit AuctionEnded(s_highestBidder, s_highestBid);
-        // emit AuctionEnded(s_tokenIdOnAuction, s_fractionalizedAmountOnAuction, s_highestBidder, s_highestBid);
     }
 
-    // 接收NFT 完成
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data)
+    function cancelAuction() external {
+        if (msg.sender != i_seller) revert OnlySellerCanCancel();
+        if (s_ended) revert Error_AuctionEnded();
+
+        s_ended = true;
+        IERC721(i_ERC721).safeTransferFrom(address(this), i_seller, i_tokenId, "");
+
+        emit AuctionCancelled();
+    }
+
+    function onERC721Received(address operator, address from, uint256 _tokenId, bytes calldata data)
         external
+        override
         returns (bytes4)
     {
-        if (msg.sender != address(i_ERC721)) {
-            revert OnlyERC721Supported();
-        }
+        if (msg.sender != i_ERC721) revert OnlyERC721Supported();
+        if (_tokenId != i_tokenId) revert InvalidTokenId();
 
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    // 查询是否支持接口  完成
     function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165) returns (bool) {
         return interfaceId == type(IERC721Receiver).interfaceId || interfaceId == type(IERC165).interfaceId;
     }
